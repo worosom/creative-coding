587655aec45c719ed1a11c5501873547d635355a
worosom <alexander.morosow@gmail.com>
2017-04-03 21:35:58 +0200

    add javascript for tumblr

---
+++ cc2/tumblr_theme/tumblr_theme.js
@@ -0,0 +1,17 @@
+var two;
+function setup() {
+  var canvas = createCanvas(80, 80, WEBGL);
+  canvas.parent('title-sketch');
+  two = loadModel("https://raw.githubusercontent.com/worosom/creative-coding/master/cc2/assets/2.obj");
+}
+
+function draw() {
+  background(255);
+  var dirX = (mouseX / windowWidth)-.5;
+  var dirY = (mouseY / windowHeight)-.5;
+  translate(0, 0, -66);
+  rotateX(PI);
+  rotateX(dirX*TWO_PI);
+  rotateY(dirY*TWO_PI);
+  model(two);
+}
